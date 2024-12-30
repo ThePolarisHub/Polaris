bcc5a086d39f8a1b4c2f47f9585dcb186b75e144
iamjamesjohn <messinghamj@gmail.com>
2024-12-30 00:31:57 +0000

    feature: added supabase package for clients and mutations

---
+++ packages/supabase/src/clients/server.ts
@@ -0,0 +1,86 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies, headers } from "next/headers";
+
+const conWarn = console.warn;
+const conLog = console.log;
+
+const IGNORE_WARNINGS = [
+	"Using the user object as returned from supabase.auth.getSession()",
+];
+
+console.warn = (...args) => {
+	const match = args.find((arg) =>
+		typeof arg === "string"
+			? IGNORE_WARNINGS.find((warning) => arg.includes(warning))
+			: false,
+	);
+	if (!match) {
+		conWarn(...args);
+	}
+};
+
+console.log = (...args) => {
+	const match = args.find((arg) =>
+		typeof arg === "string"
+			? IGNORE_WARNINGS.find((warning) => arg.includes(warning))
+			: false,
+	);
+	if (!match) {
+		conLog(...args);
+	}
+};
+
+type CreateClientOptions = {
+	admin?: boolean;
+	schema?: "public" | "storage";
+};
+
+export async function createClient(options?: CreateClientOptions) {
+	const { admin = false, ...rest } = options ?? {};
+
+	const cookieStore = await cookies();
+	const headersList = await headers();
+
+	const key = admin
+		? (process.env.SUPABASE_SERVICE_KEY as string)
+		: (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string);
+
+	const auth = admin
+		? {
+				persistSession: false,
+				autoRefreshToken: false,
+				detectSessionInUrl: false,
+			}
+		: {};
+
+	return createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+		key,
+		{
+			...rest,
+			cookies: {
+				getAll() {
+					return cookieStore.getAll();
+				},
+				setAll(cookiesToSet) {
+					try {
+						for (const { name, value, options } of cookiesToSet) {
+							cookieStore.set(name, value, options);
+						}
+					} catch {
+						// The `setAll` method was called from a Server Component.
+						// This can be ignored if you have middleware refreshing
+						// user sessions.
+					}
+				},
+			},
+			auth,
+			global: {
+				headers: {
+					// Pass user agent from browser
+					"user-agent": headersList.get("user-agent") as string,
+				},
+			},
+		},
+	);
+}
