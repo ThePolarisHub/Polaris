e796abdf02e01e35606c40f6367b6ac281ca333a
iamjamesjohn <messinghamj@gmail.com>
2024-12-31 08:57:03 +0000

    chore: skip env validation during CI/CD builds

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -14,4 +14,5 @@ export const env = createEnv({
 		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 		SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
 	},
+	skipValidation: !!process.env.CI,
 });
